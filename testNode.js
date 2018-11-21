5c524b4f1345e25df67b3541899476fd8be426a7
fcic <fcic@qq.com>
2018-11-21 22:13:07 +0800

    testNode2
    Signed-off-by: fcic <fcic@qq.com>

--- testNode.js
+++ testNode.js
@@ -1,6 +1,8 @@
-var http = require(http);
-http.createServer(function (req, res) { 
-    res.writeHead("200", {"Content-Type": "text/plain"}); 
-    res.end("Hello World hahah"); 
-}).listen("1337", "127.0.0.1"); 
-//console.log(Server running at http://127.0.0.1:1337/);    
+var http = require("http");
+
+http.createServer(function(req,res){
+   res.writeHead(200,{"Content-type":"text/blain"});
+   res.write("Hello NodeJs");
+   res.write("test2");
+   res.end();
+}).listen(8888);
