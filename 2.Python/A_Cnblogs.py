9c9509847fca63162467b1d6082aba097024783b
fcic <fcic@qq.com>
2019-07-21 23:53:23 +0800

    new
    Signed-off-by: fcic <fcic@qq.com>

--- 2.Python/A_Cnblogs.py
+++ 2.Python/A_Cnblogs.py
@@ -6,8 +6,34 @@ def getData(url,xpath):
     html = B1_requests.Gethtml(url)
     #print(html)
     #res= etree.HTML(html).xpath('/html/body/div[1]/p[2]/a/text()')
-    res = etree.HTML(html).xpath(xpath)
-    for r in res:
-        print(r)
+    #row = []
+    rows= []
+    obj = etree.HTML(html).xpath('//*[@id="post_list"]/div/div[2]')
+    for r in obj:
+        row = [r.xpath('h3/a/text()'),r.xpath('div/a/text()'),r.xpath('p/text()'),r.xpath('div/span[1]/a/text()'),r.xpath('div/span[2]/a/text()')]
+        rows.append(row)
+    #print(rows)
+    return rows
+#title: 
+# //*[@id="post_list"]/div[1]/div[2]/h3/a
+#author: 
+# //*[@id="post_list"]/div[1]/div[2]/div/a
+#content: 
+# //*[@id="post_list"]/div[1]/div[2]/p/text()
+#comment: 
+# //*[@id="post_list"]/div[1]/div[2]/div/span[1]/a
+#read: 
+# //*[@id="post_list"]/div[1]/div[2]/div/span[2]/a
+#datetime: 
+# //*[@id="post_list"]/div[1]/div[2]/div/text()
+#recommand: 
+# //*[@id="digg_count_11223205"]
 
-getData('https://www.cnblogs.com/','//*[@id="post_list"]/div/div[2]/h3/a/text()')
+#getData('https://www.cnblogs.com/','//*[@id="post_list"]/div/div[2]/h3/a/text()')
+
+def write2(file,content):  
+    f=open(file,'a')
+    f.write(content)
+    f.close()
+
+write2('./test.txt', ''.join( getData('https://www.cnblogs.com/','//*[@id="post_list"]/div/div[2]/h3/a/text()')))
