f5507c53e730a036677930d96bcdf159a0ceda4c
fcic <fcic@qq.com>
2019-07-27 23:27:15 +0800

    new
    Signed-off-by: fcic <fcic@qq.com>

--- 2.Python/D2_Json.py
+++ 2.Python/D2_Json.py
@@ -7,4 +7,8 @@ json_1row_Obj = json.loads(json_1row_Str)
 
 #Json Obj
 json_1row_Obj2 = { "Id":"fcic" ,     "age":30}
-print( json_1row_Obj2['age'] )
+#print( json_1row_Obj2['age'] )
+
+
+json_Nrow_Obj2 = [{ "Id":"fcic" ,     "age":30},{"Id":"ooobbb","age":31} ] 
+print( json_Nrow_Obj2[1]['age'] )
