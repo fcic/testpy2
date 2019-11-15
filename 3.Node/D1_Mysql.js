6aa91d51d3be506d5bd3c49f896c6999d4af80b2
fcic <fcic@qq.com>
2019-11-16 00:18:11 +0800

    new
    Signed-off-by: fcic <fcic@qq.com>

--- 3.Node/D1_Mysql.js
+++ 3.Node/D1_Mysql.js
@@ -1,18 +1,22 @@
+// mysql默认协议报错ER_NOT_SUPPORTED_AUTH_MODE
+// ans: alert user 'root'@'localhost' identified with mysql_native_password by 'fcic123456';
+
+//PS: create new
 // CREATE USER 'fcic'@'%' IDENTIFIED BY 'fcic123456';
 // GRANT ALL PRIVILEGES ON *.* TO 'fcic'@'localhost';
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'fcic',
+  user     : 'root',
   password : 'fcic123456',
   database : 'test'
 });
  
 connection.connect();
  
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+connection.query('SELECT * from websites', function (error, results, fields) {
   if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+  console.log( JSON.stringify( results));
 });
 
