025d27baa277acd38cbabc63aa321fd62429633d
Lian Duan <fcic@qq.com>
2019-11-22 08:38:45 +0000

    n

--- 3.Node/B2_Child_process.js
+++ 3.Node/B2_Child_process.js
@@ -1,4 +1,16 @@
-var child_process = require('child_process');
-var sh = child_process.exec('ls', function (err, stdout, stderr) {
+//fcic: linux 和win可以用这个
+
+var cmd = require('child_process');
+
+
+cmd.exec('ls', function (err, stdout, stderr) {
     console.log(stdout);
 });
+
+
+
+cmd.exec('curl ip.sb', function (err, stdout, stderr) {
+    console.log(stdout);
+});
+
+
